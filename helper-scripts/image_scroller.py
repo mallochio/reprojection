08a1f9b9aa7082e69c81409708f24f14a0dfee37
mallochio <siddsantham@gmail.com>
2022-12-27 22:08:11 +0100

    minor

--- helper-scripts/image_scroller.py
+++ helper-scripts/image_scroller.py
@@ -17,23 +17,28 @@ def update_image(window, image):
     window["image"].update(data=cv2.imencode(".png", image)[1].tobytes())
 
 
-# Create a thread to load the images in the background
 def image_loader_thread(image_paths, image_queue):
+    """ Loads images in the background and puts them in a queue."""
     for path in image_paths:
         image = load_image(path)
         image_queue.put(image)
 
 
 def main(image_list):
+    """
+    Creates a GUI window that displays images from a list of image file paths, and also allows the user to scroll through the images using the arrow keys.
+    """
     # Read in list of image file paths from text file
     with open(image_list, "r") as f:
         image_paths = f.read().splitlines()
 
-    # Create a PySimpleGUI window
+    # # Create a PySimpleGUI window
     layout = [
         [sg.Image(filename="", key="image")],
         [sg.Button("Previous"), sg.Button("Next")],
     ]
+
+    # # Create a PySimpleGUI window
     window = sg.Window(
         "Image Viewer",
         layout,
@@ -53,7 +58,7 @@ def main(image_list):
         event, values = window.read(timeout=100)
         if event in (None, "Exit"):
             break
-        elif event == "Next" or event == "window:down:40":
+        elif event in ["Next", "window:down:40"]:
             index = (index + 1) % len(image_paths)
             try:
                 # Try to get the next image from the queue
@@ -62,7 +67,7 @@ def main(image_list):
                 # If the queue is empty, use a placeholder image
                 image = cv2.imread("placeholder.png")
             update_image(window, image)
-        elif event == "Previous" or event == "window:down:38":
+        elif event in ["Previous", "window:down:38"]:
             index = (index - 1) % len(image_paths)
             try:
                 # Try to get the previous image from the queue
@@ -81,7 +86,7 @@ if __name__ == "__main__":
         "--image_list",
         type=str,
         required=True,
-        help=f"Path to the text file containing the list of images to scroll through",
+        help="Path to the text file containing the list of images to scroll through",
     )
     args = parser.parse_args()
     main(args.image_list)
