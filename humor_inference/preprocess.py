3c14ae3b17055455f0e73d92268186f8f90e5de2
mallochio <siddsantham@gmail.com>
2023-02-24 14:24:29 +0100

    Change arguments to reflect existing folder structuring per sequence, other minor edits

--- humor_inference/preprocess.py
+++ humor_inference/preprocess.py
@@ -16,9 +16,9 @@ from typing import List
 
 import cv2
 import numpy as np
-from scipy.stats import mode as scipy_mode
 from scipy.special import kl_div as kl_div
 from tqdm import tqdm
+
 
 def compute_body_coverage(seg_mask, iuv_map, debug=False):
     part_index = {
@@ -55,14 +55,21 @@ def compute_body_coverage(seg_mask, iuv_map, debug=False):
         intersection = body_part_pixels * body_pixels
 
         # Compute the weighted surface area of the body part
-        part_surface_areas[part] = max(50, intersection.sum())/body_pixels.sum()
+        part_surface_areas[part] = max(50, intersection.sum()) / body_pixels.sum()
         if debug:
             print(f"{part}: {part_surface_areas[part]}px")
 
-    ideal_distribution = {"torso": 0.4, "head": 0.2, "left_leg":0.1, "right_leg": 0.1,
-                          "upper_left_arm": 0.05, "upper_right_arm": 0.05, "lower_left_arm": 0.05,
-                          "lower_right_arm": 0.05}
-    computed_distribution = [part_surface_areas[part] for part in ideal_distribution.keys()]
+    ideal_distribution = {
+        "torso": 0.4,
+        "head": 0.2,
+        "left_leg": 0.1,
+        "right_leg": 0.1,
+        "upper_left_arm": 0.05,
+        "upper_right_arm": 0.05,
+        "lower_left_arm": 0.05,
+        "lower_right_arm": 0.05,
+    }
+    computed_distribution = [part_surface_areas[part] for part in ideal_distribution]
     div = sum(kl_div(list(ideal_distribution.values()), computed_distribution))
     if debug:
         print(ideal_distribution, computed_distribution)
@@ -74,6 +81,7 @@ def compute_body_coverage(seg_mask, iuv_map, debug=False):
         print(f"diversity: {diversity_metric}")
     return diversity_metric
 
+
 def save_subsequence(
     subsequence: List, subsequence_idx: int, contains_person: bool, output_path: str
 ):
@@ -84,7 +92,7 @@ def save_subsequence(
         subsequence_idx (int): Index of the sub-sequence.
         output_path (str): Path to the output directory.
     """
-    print("[*] Saving sub-sequence {}...".format(subsequence_idx))
+    print(f"[*] Saving sub-sequence {subsequence_idx}...")
     # Create the output directory for the sub-sequence
     subsequence_path = os.path.join(
         output_path,
@@ -104,7 +112,7 @@ def main(
     iuvs_path: str,
     output_path: str,
     threshold: float = 0.5,
-    debug: bool = False
+    debug: bool = False,
 ):
     """Preprocess a sequence.
 
@@ -117,16 +125,16 @@ def main(
     MIN_FRAMES_FOR_PERSON = 30
     # Load all image absolute file paths in the sequence which follows this tree structure:
     # <sequence_path>/rgb/<frame_id>.jpg
-    rgb_files = sorted(
-        [
-            os.path.join(sequence_path, "rgb", f)
-            for f in os.listdir(os.path.join(sequence_path, "rgb"))
-        ]
-    )
+    rgb_path = os.path.join(sequence_path, "rgb")
+    rgb_files = sorted([os.path.join(rgb_path, f) for f in os.listdir(rgb_path)])
+    if not masks_path:
+        masks_path = os.path.join(sequence_path, "rgb_dp2_mask")
     # Load all mask file names in the sequence which follows this tree structure:
     # <masks_path>/<frame_id>.jpg
     mask_files = sorted(os.listdir(masks_path))
 
+    if not iuvs_path:
+        iuvs_path = os.path.join(sequence_path, "rgb_dp2_iuv")
     # Load all IUV file names in the sequence which follows this tree structure:
     # <iuvs_path>/<frame_id>.jpg
     iuv_files = sorted(os.listdir(iuvs_path))
@@ -169,11 +177,11 @@ def main(
     contains_person: bool = body_part_coverage > threshold
     median_filter_size = 5
     median_filter = [body_part_coverage] * median_filter_size
-    for rgb_file, mask_file, iuv in tqdm(
+    for rgb_file, mask_file, iuv_file in tqdm(
         zip(rgb_files, mask_files, iuv_files), total=len(rgb_files)
     ):
         mask = cv2.imread(os.path.join(masks_path, mask_file), cv2.IMREAD_GRAYSCALE)
-        iuv = cv2.imread(os.path.join(iuvs_path, iuv))
+        iuv = cv2.imread(os.path.join(iuvs_path, iuv_file))
         body_part_coverage = compute_body_coverage(mask, iuv, debug)
         median_filter.pop(0)
         median_filter.append(body_part_coverage)
@@ -190,33 +198,46 @@ def main(
                 contains_person = True
                 current_subsequence = []
                 subsequence_idx += 1
-            # Add the frame to the sub-sequence
-            current_subsequence.append(rgb_file)
-        else:
-            # If the subject was visible in the previous frame, save the sub-sequence
-            if contains_person:
-                # If the subject was only visible for less than MIN_FRAMES_FOR_PERSON though,
-                # reject the sequence and consider the subject not visible.
-                contains_person = len(current_subsequence) >= MIN_FRAMES_FOR_PERSON
-                save_subsequence(
-                    current_subsequence, subsequence_idx, contains_person, output_path
-                )
-                contains_person = False
-                current_subsequence = []
-                subsequence_idx += 1
-            # Add the frame to the sub-sequence
-            current_subsequence.append(rgb_file)
+        elif contains_person:
+            # If the subject was only visible for less than MIN_FRAMES_FOR_PERSON though,
+            # reject the sequence and consider the subject not visible.
+            contains_person = len(current_subsequence) >= MIN_FRAMES_FOR_PERSON
+            save_subsequence(
+                current_subsequence, subsequence_idx, contains_person, output_path
+            )
+            contains_person = False
+            current_subsequence = []
+            subsequence_idx += 1
+        # Add the frame to the sub-sequence
+        current_subsequence.append(rgb_file)
     # Save the last sub-sequence if there's one remaining
     if contains_person:
         contains_person = len(current_subsequence) >= MIN_FRAMES_FOR_PERSON
     save_subsequence(current_subsequence, subsequence_idx, contains_person, output_path)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess a sequence.")
-    parser.add_argument("sequence", type=str, help="The RGB sequence to preprocess.")
-    parser.add_argument("masks", type=str, help="The segmentation masks of the sequence.")
-    parser.add_argument("iuvs", type=str, help="The IUV maps of the sequence.")
-    parser.add_argument("--output", type=str, help="The output directory.", required=True)
+    parser.add_argument(
+        "sequence",
+        type=str,
+        help="The folder containing the sequence to preprocess (containing a directory with RGB files).",
+    )
+    parser.add_argument(
+        "--masks",
+        type=str,
+        required=False,
+        help="The segmentation masks of the sequence, usually inside the sequence folder else explicitly linked.",
+    )
+    parser.add_argument(
+        "--iuvs",
+        type=str,
+        required=False,
+        help="The IUV maps of the sequence, usually inside the sequence folder else explicitly linked.",
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, help="The output directory.", required=True
+    )
     parser.add_argument(
         "--threshold",
         type=float,
