Fix error computation of omnidir calib

calibration/opencv_calibration/calibrate_mono.py
@@ -48,10 +48,9 @@ def calibrate_cam(
     # termination criteria
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 200, 1e-6)
     fisheye_calibration_flags = (
-        None
-        # cv.fisheye.CALIB_RECOMPUTE_EXTRINSIC
-        # + cv.fisheye.CALIB_CHECK_COND
-        # + cv.fisheye.CALIB_FIX_SKEW
+        cv.omnidir.CALIB_FIX_SKEW
+        + cv.omnidir.CALIB_USE_GUESS
+        + cv.omnidir.CALIB_FIX_CENTER
     )
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((1, grid_size[0] * grid_size[1], 3), np.float32)
@@ -91,9 +90,10 @@ def calibrate_cam(
         f"[*] Found {len(objpoints)} valid frames! Calibrating for a {img_shape[0]}x{img_shape[1]} resolution..."
     )
     xi = None
+    rms = None
     if fish_eye:
         (
-            ret,
+            rms,
             cam_mat,
             xi,
             dist_coeffs,
@@ -104,13 +104,13 @@ def calibrate_cam(
             objpoints,
             imgpoints,
             img_shape[::-1],
-            None,
-            None,
-            None,
-            None,
+            K=None,
+            xi=None,
+            D=None,
+            flags=0,#fisheye_calibration_flags,
             criteria=(
                 cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER,
-                200,
+                1000,
                 1e-6,
             ),
         )
@@ -144,6 +144,7 @@ def calibrate_cam(
     mean_error = 0
     if fish_eye:
         assert idx is not None
+        print(f"-> RMS: {rms:.2f}px")
         idx = idx.squeeze()
         used = 0
         for i in range(len(idx)):
@@ -151,18 +152,13 @@ def calibrate_cam(
                 used += 1
                 xi = xi.item() if isinstance(xi, np.ndarray) else xi
                 imgpoints2, _ = cv.omnidir.projectPoints(
-                    objpoints[i],
-                    rotation_vec[i],
-                    translation_vec[i],
-                    cam_mat,
-                    xi,
-                    dist_coeffs,
+                    objpoints[idx[i]], rotation_vec[i], translation_vec[i], cam_mat, xi, dist_coeffs
                 )
-                imgpoints2 = np.swapaxes(imgpoints2, 0, 1)
-                error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
+                imgpoints2 = imgpoints2.swapaxes(0, 1)
+                error = cv.norm(imgpoints[idx[i]], imgpoints2, cv.NORM_L2) / len(imgpoints2)
                 mean_error += error
-        print(f"-> Used {used}/{len(objpoints)} frames for calibration.")
         print(f"-> Total error: {mean_error/used:.2f}px")
+        print(f"-> Used {used}/{len(objpoints)} frames for calibration.")
     else:
         for i in range(len(objpoints)):
             imgpoints2, _ = cv.projectPoints(