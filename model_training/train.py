44da1d6f2aa001f32c00aace64c5681876603273
sid <siddsantham@gmail.com>
2024-03-08 14:31:00 +0100

    Change training code to use transformers and the full dataset (corrected code)

--- model_training/train.py
+++ model_training/train.py
@@ -11,8 +11,8 @@
 '''
 import os
 import json
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
+import lightning as pl
+from lightning.pytorch import Trainer
 import torch
 import torch.nn as nn
 import numpy as np
@@ -24,13 +24,32 @@ from PIL import Image
 import smplx
 import trimesh
 import argparse
+from lightning.pytorch.loggers import WandbLogger
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
+
+
+torch.set_float32_matmul_precision('high')  # or 'medium'
 
 SMPL_MODEL_PATH = '/home/sid/Projects/WHAM/dataset/body_models/smpl/SMPL_NEUTRAL.pkl'
+
+def custom_collate_fn(batch):
+    images = [item for sublist in [x[0] for x in batch] for item in sublist]
+    projected_vertices = [item for sublist in [x[1] for x in batch] for item in sublist]
+    target_vertices = [item for sublist in [x[2] for x in batch] for item in sublist]
+
+    # Pad the sequences with zeros so they have the same length and can be batched
+    images = pad_sequence(images, batch_first=True, padding_value=0)
+    projected_vertices = pad_sequence(projected_vertices, batch_first=True, padding_value=0)
+    target_vertices = pad_sequence(target_vertices, batch_first=True, padding_value=0)
+    return images, projected_vertices, target_vertices
+
 
 def load_image(image_path):
     image = Image.open(image_path).convert('RGB')  # Open as RGB
     image = apply_image_transformations(image)  # Preprocessing function
     return image 
+
 
 def apply_image_transformations(image):
     transform = T.Compose([
@@ -41,42 +60,55 @@ def apply_image_transformations(image):
     ])
     return transform(image)
 
+
 def calculate_pose_smoothness(output_poses):
     # Simple temporal smoothness - difference between consecutive frames
     smoothness_loss = torch.mean(torch.norm(output_poses[:-1] - output_poses[1:], dim=(1, 2))) 
     return smoothness_loss
+
 
 class PoseDataset(torch.utils.data.Dataset):
     def __init__(self, json_file, base_dir):
         with open(json_file, 'r') as f:
             self.data = json.load(f)
         self.base_dir = base_dir
+        self.all_files = self.prepare_dataset()
+
+    def prepare_dataset(self):
+        all_files = []
+        for item in self.data:
+            image_folder = os.path.join(self.base_dir, item['omni'])
+            pose_folder = os.path.join(self.base_dir, item['pose'])
+            mesh_folder = os.path.join(self.base_dir, item['mesh'])
+
+            image_files = sorted(os.listdir(image_folder))
+            pose_files = sorted(os.listdir(pose_folder))
+            mesh_files = sorted(os.listdir(mesh_folder))
+
+            # pair each image file with corresponding pose and mesh file
+            all_files.extend(
+                {
+                    'image': os.path.join(image_folder, img_file),
+                    'pose': os.path.join(pose_folder, pose_file),
+                    'mesh': os.path.join(mesh_folder, mesh_file),
+                }
+                for img_file, pose_file, mesh_file in zip(
+                    image_files, pose_files, mesh_files
+                )
+            )
+        return all_files
 
     def __len__(self):
-        return len(self.data)
+        return len(self.all_files)
 
     def __getitem__(self, index):
-        item = self.data[index]
-        image_folder = os.path.join(self.base_dir, item['omni'])
-        image_files = os.listdir(image_folder)
-        image_path = os.path.join(image_folder, image_files[0])  # Assuming the first image in the folder
-        image = load_image(image_path)  
+        files = self.all_files[index]
+        image = load_image(files['image'])  # Load a single image
+        poses = torch.from_numpy(np.load(files['pose'])).float()  # Load a single pose
+        target_mesh = trimesh.load(files['mesh'], process=False)  # Load a single mesh
+        target_vertices = torch.tensor(target_mesh.vertices).float()  # Convert mesh to tensor
+        return image, poses, target_vertices
 
-        pose_folder = os.path.join(self.base_dir, item['pose'])
-        pose_files = os.listdir(pose_folder)
-        pose_path = os.path.join(pose_folder, pose_files[0])  # Assuming the first pose file in the folder
-        poses = np.load(pose_path)
-
-        projected_vertices = torch.from_numpy(poses).float()  # Convert poses to tensor
-
-        mesh_folder = os.path.join(self.base_dir, item['mesh'])
-        mesh_files = os.listdir(mesh_folder)
-        mesh_path = os.path.join(mesh_folder, mesh_files[0])  # Assuming the first mesh file in the folder
-        target_mesh = trimesh.load(mesh_path, process=False)
-
-        target_vertices = torch.tensor(target_mesh.vertices).float()
-
-        return image, projected_vertices, target_vertices
 
 class PoseEstimationModule_old(pl.LightningModule):
     def __init__(self, learning_rate=1e-4, smpl_model_path=SMPL_MODEL_PATH):
@@ -110,7 +142,6 @@ class PoseEstimationModule_old(pl.LightningModule):
         output_vertices = output_mesh.vertices
 
         return output_vertices.view(output_vertices.size(0), -1, 3)  # Reshape output vertices to (batch_size, 6890, 3)
-    
 
     def training_step(self, batch, batch_idx):
         images, projected_vertices, target_vertices = batch
@@ -130,10 +161,11 @@ class PoseEstimationModule_old(pl.LightningModule):
         # Now calculate vertex loss and projection loss
         vertex_loss = nn.MSELoss(reduction='mean')(output_vertices, target_vertices)
         projection_loss = nn.MSELoss(reduction='mean')(output_vertices[:, :, :2], projected_vertices)
+        # log vertex loss and projection loss
+        self.log("vertex_loss", vertex_loss)
+        self.log("projection_loss", projection_loss)
 
         return vertex_loss + projection_loss
-
-
 
 
 class PoseEstimationModule(pl.LightningModule):
@@ -147,7 +179,7 @@ class PoseEstimationModule(pl.LightningModule):
         
         # Remove the fully connected layer from ResNet
         self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
-        
+
         # Load a pre-trained Vision Transformer
         self.vit = ViTModel.from_pretrained(vit_name)
         self.vit_out_features = self.vit.config.hidden_size
@@ -163,18 +195,21 @@ class PoseEstimationModule(pl.LightningModule):
         # Initialize the SMPL model
         self.smpl = smplx.create(smpl_model_path, model_type='smpl')
 
+        # Projection layer to map ResNet features to the desired dimension
+        self.resnet_projection = nn.Linear(self.resnet_out_features, self.vit.config.hidden_size * (self.vit.config.image_size // self.vit.config.patch_size) ** 2)
+
     def forward(self, images):
         batch_size = images.size(0)
+
         # Extract features using the ResNet
-        resnet_features = self.resnet(images).view(batch_size, -1)
-        
-        # Prepare "fake tokens" for ViT; ViT expects a sequence, so we need to create a sequence of length 1
-        # fake_tokens = resnet_features.unsqueeze(1)
-        vit_tokens = resnet_features.view(batch_size, 1, self.resnet_out_features)
+        resnet_features = self.resnet(images)
+        resnet_features = resnet_features.view(batch_size, -1)
+
+        # Create dummy pixel values for ViT (or use actual image patches)
+        dummy_pixel_values = torch.zeros(batch_size, 3, self.vit.config.image_size, self.vit.config.image_size, device=images.device)
         
         # Extract features using the ViT
-        # vit_outputs = self.vit(pixel_values=fake_tokens)
-        vit_outputs = self.vit(pixel_values=vit_tokens)
+        vit_outputs = self.vit(pixel_values=dummy_pixel_values)
         vit_features = vit_outputs.last_hidden_state[:, 0, :]
         
         # Combine features from ResNet and ViT
@@ -219,16 +254,18 @@ class PoseEstimationModule(pl.LightningModule):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--json-file", default="", type=str, required=True)
-    parser.add_argument("--base-dir", default="", type=str, required=True)
+    parser.add_argument("--json-file", default="/home/sid/Projects/NAS-mountpoint/train.json", type=str)
+    parser.add_argument("--base-dir", default="/home/sid/Projects/NAS-mountpoint", type=str)
     args = parser.parse_args()
 
-    JSON_FILE = args.json_file
-    BASE_DIR = args.base_dir
-
-    dataset = PoseDataset(JSON_FILE, BASE_DIR)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=False)
+    dataset = PoseDataset(args.json_file, args.base_dir)
+    dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=20, pin_memory=True)
 
     model = PoseEstimationModule()
-    trainer = Trainer(max_epochs=10)  
+    wandb_logger = WandbLogger(log_model="all", project="Hades-krypton", entity="pyre")
+    trainer = Trainer(
+        max_epochs=100,
+        logger=wandb_logger,
+    )
     trainer.fit(model, dataloader)
+    torch.cuda.empty_cache()
